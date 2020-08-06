1c6d265b8256418bb22e2ad6657cf915080ecc03
bsyzch <47052601+bsyzch@users.noreply.github.com>
2020-08-07 00:49:00 +0900

    구현

---
+++ 구현1_1.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+moves = input().split()
+move_types=['L', 'R', 'U', 'D']
+x, y = 1, 1
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+for move in moves:
+    for i in range (len(move_types)):
+        if move == move_types[i]:
+            nx = x+dx[i]
+            ny = y+dy[i]
+    if nx<1 or nx>n or ny<1 or ny>n:
+        print("Out of Range!")
+        exit()
+        break
+        #continue
+    x,y = nx, ny
+print (x,y)
